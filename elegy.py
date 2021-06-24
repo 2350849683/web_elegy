b322e6e562a0d1591fc7dd931272695315c09640
silentmx <2350849683@qq.com>
2021-06-24 19:04:29 +0800

    框架处理请求升级

--- elegy.py
+++ elegy.py
@@ -1,16 +1,63 @@
 from wsgiref.simple_server import make_server
 
+
+class Elegy(object):
+
+    def __init__(self):
+        self.config = {
+            "port" :8000,
+            "host" :""
+        }
+
+    def run(self):
+        run_dev(self.config["host"],self.config["port"])
+
+    def process_func(self,environ):
+        response=ElegyResponse()
+        return response.render(environ["PATH_INFO"])
+
+
+class ElegyResponse(object):  #获得的响应
+    def __init__(self):
+        self.status_codes = {
+            200: '200 OK',
+            404: '404 Not Found',
+        }
+        self.route_dict = {
+            '/': self.index,
+            '/hello': self.hello
+        }
+
+    def index(self):
+        return [b"OK"]
+
+    def hello(self):
+        return [b"hello"]
+
+    def elegy_404(self):
+        return [b"404"]
+
+    def render(self,env):
+        if env in self.route_dict:
+            body = self.route_dict[env]()
+            status = self.status_codes[200]
+        else:
+            body = self.elegy_404()
+            status = self.status_codes[404]
+        return body, status
+
 def application(environ, start_response):
+    elegy=Elegy()
+    body, status = elegy.process_func(environ)
+
+    start_response(status, [('Content-type', 'text/plain')])
+
+    return body
 
 
-    start_response('200 OK', [('Content-type', 'text/plain')])
-    return  [b"OK"]
+def run_dev(host,port):
+    with make_server(host,port,application) as httpd:
+        print("Serving on port 8000...")
+        httpd.serve_forever()
 
-
-
-with make_server('', 8000,application) as httpd:
-    print("Serving on port 8000...")
-    httpd.serve_forever()
-
-
-
+Elegy().run()
